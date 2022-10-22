253ef95f650803acad14694518e505e2dc353402
Jan Matuszewski <matuszewski.jan@hotmail.com>
2022-10-22 19:45:29 +0200

    First working version of code execution

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,60 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const cp = require('child_process');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let testsOutput = vscode.window.createOutputChannel("Tests");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rails-test-runner" is now active!');
+	let onFileSave = vscode.workspace.onDidSaveTextDocument(async (document: vscode.TextDocument) => {
+		if (document.languageId === "ruby" && document.uri.scheme === "file") {
+			const workspaceFolder = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0]
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rails-test-runner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Rails Test Runner!');
+			if (!workspaceFolder) {
+				vscode.window.showErrorMessage("Rails Automatic Test runner won't work without Workspace");
+				return;
+			}
+
+			const workspacePath = workspaceFolder.uri.path;
+			const documentAbsolutePath = document.uri.path;
+			const documentRelativePath = documentAbsolutePath.replace(workspacePath, '');
+			const relativeTestPath = documentRelativePath.replace('/app/', '/spec/').replace('.rb', '_spec.rb');
+			const absoluteTestPath = workspacePath + relativeTestPath;
+
+			let testExists;
+			try {
+				await vscode.workspace.fs.stat(vscode.Uri.file(absoluteTestPath))
+				testExists = true;
+			} catch(e) {
+				testExists = false;
+			}
+
+			if (testExists) {
+				testsOutput.clear()
+				cp.exec(
+					`bundle exec rspec ./${relativeTestPath}`,
+					{ timeout: 10000, cwd: workspacePath },
+					(err: string, stdout: string, stderr:string) => {
+						if (stdout) {
+							testsOutput.appendLine(stdout);
+						} else if (stderr) {
+							testsOutput.appendLine(stdout);
+						} else if (err) {
+							vscode.window.showErrorMessage('Rails Automatic Test runner error: ' + err);
+						}
+
+						testsOutput.show();
+					}
+				);
+			} else {
+				if (!documentAbsolutePath.includes('/spec')) {
+					vscode.window.showErrorMessage("Rails Automatic Test runner: no corresponding test file found.");
+				}
+			}
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(onFileSave);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
